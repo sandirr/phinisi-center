eef3deedf8d8556ae9868fb99ba5c30e5ec0fcda
Andi Irsandi <andi.irsandi765@gmail.com>
2023-03-06 14:02:48 +0800

    notif click

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,3 +1,5 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable no-plusplus */
 /* eslint-disable no-restricted-globals */
 /* eslint-disable no-undef */
 // are not available in the service worker.
@@ -43,4 +45,30 @@ messaging.onBackgroundMessage((payload = {}) => {
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
+
+  self.addEventListener('notificationclick', (event) => {
+    console.log('On notification click: ', event.notification.tag);
+    // Android doesn't close the notification when you click on it
+    // See: http://crbug.com/463146
+    event.notification.close();
+
+    // This looks to see if the current is already open and
+    // focuses if it is
+    event.waitUntil(
+      clients.matchAll({
+        type: 'window',
+      })
+        .then((clientList) => {
+          for (let i = 0; i < clientList.length; i++) {
+            const client = clientList[i];
+            if (client.url == '/' && 'focus' in client) { return client.focus(); }
+          }
+          if (clients.openWindow) {
+            return clients.openWindow('/');
+          }
+
+          return null;
+        }),
+    );
+  });
 });
